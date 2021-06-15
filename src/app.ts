569c350d64e8110c88329985ed599aaaaea6d848
FireChargeReal <48412324+FireChargeReal@users.noreply.github.com>
2021-06-16 00:39:29 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ export default class HelloWorld {
 
 		this.menu = MRE.Actor.Create(this.context, {});
 
-		
+		this.create_score();
 
 		this.buzzerSound = this.assets.createSound(
 			'alarmSound',
